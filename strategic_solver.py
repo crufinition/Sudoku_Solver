d8017beb9cbe9a64490db72d6a70f03e33413da7
myaccount0102 <crufinition@gmail.com>
2026-03-23 18:40:07 -0700

    Update strategic_solver.py

--- strategic_solver.py
+++ strategic_solver.py
@@ -50,30 +50,66 @@ def last_free(puzzle, candidates):
             new_candidates[idx] = []
     return new_puzzle, new_candidates
 
+def hidden(puzzle, candidates):
+    '''Insert the candidate that appears in exactly one position in a row, column, or grid
+
+    Containind strategies: Cross Hatching, Hidden Single'''
+    new_puzzle = deepcopy(puzzle)
+    new_candidates = deepcopy(candidates)
+    groups = [col_idx(i) for i in range(9)]\
+            +[row_idx(9*i) for i in range(9)]\
+            +[grid_idx(9*3*r+3*c) for r in range(3) for c in range(3)]
+    for group in groups: # check rows, columns, and grids
+        for i in range(1,10):
+            num = str(i)
+            found = False
+            for idx in group:
+                if num in new_candidates[idx]:
+                    if not found:
+                        found = True
+                        the_idx = idx
+                    else:
+                        found = False
+                        break
+            if found:
+                new_puzzle[the_idx] = num
+                new_candidates[idx] = []
+    return new_puzzle, new_candidates
+
 #### END OF STRATEGIES
 
 
 def strategic(puzzle, show=False):
     candidates = [[str(i) for i in range(1,10)] for idx in range(81)]
-    count = 1
+    count = 0
     while True:
         candidates = clean_candidates(puzzle, candidates, show)
         new_puzzle, new_candidates = last_free(puzzle, candidates)
-        if new_puzzle == puzzle:
-            if show:
-                print('End of solving without assumptions')
-            return new_puzzle, new_candidates
-        else:
+        #new_puzzle, new_candidates = puzzle, candidates
+        if new_puzzle != puzzle:
+            count += 1
             puzzle = new_puzzle
             candidates = new_candidates
             if show:
                 print(f'Progressed count: {count}')
-            count += 1
+        else:
+            new_puzzle, new_candidates = hidden(puzzle, candidates)
+            if new_puzzle != puzzle:
+                count += 1
+                puzzle = new_puzzle
+                candidates = new_candidates
+            else:
+                if show:
+                    print('End of solving without assumptions')
+                return new_puzzle, new_candidates
+
 
 n = 10000
 select_puzzle = pd.read_sql(f'SELECT * FROM sudoku LIMIT {n}', db)
 
 #puzzle = select_puzzle['puzzle'][0]
+#puzzle = list(puzzle)
+#puzzle[47] = '6'
 #solution = select_puzzle['solution'][0]
 
 #print_puzzle(solution, "Solution")
@@ -95,4 +131,7 @@ for i in range(n):
     if result==list(solution):
         count += 1
 
-print(f'Success rate: {count/n}')
+print(f'Total number of puzzles : {n}')
+print(f'Successfully solved     : {count}')
+print(f'Success rate            : {count/n}')
+#print_puzzle(result)
